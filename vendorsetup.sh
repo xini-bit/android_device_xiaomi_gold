e0321dd7d3fa793be87224152c4c1178366e2902
xini-bit <xinight@protonmail.com>
2026-07-12 07:57:51 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_gold-user
-add_lunch_combo omni_gold-userdebug
-add_lunch_combo omni_gold-eng
+add_lunch_combo twrp_gold-user
+add_lunch_combo twrp_gold-userdebug
+add_lunch_combo twrp_gold-eng
