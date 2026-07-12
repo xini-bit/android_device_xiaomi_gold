00a7e5a189eee080c7e3285bec03982db24e3c59
xini-bit <xinight@protonmail.com>
2026-07-13 01:06:26 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.mt6833
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6833 \
     libgptutils \
     libz \
